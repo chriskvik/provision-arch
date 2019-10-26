90d079468a27a5c3a75ecb41fc9c998eaa742308
chriskvik <christian@chk.no>
2019-10-27 01:24:12 +0200

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,5 +1,5 @@
 curl -sL https://raw.githubusercontent.com/chriskvik/provision-arch/master/arch-setup.sh > setup.sh
-curl -sL https://github.com/chriskvik/provision-arch/blob/master/configure.sh > configure.sh
-curl -sL https://github.com/chriskvik/provision-arch/blob/master/my.conf > my.conf
+curl -sL https://raw.githubusercontent.com/chriskvik/provision-arch/master/configure.sh > configure.sh
+curl -sL https://raw.githubusercontent.com/chriskvik/provision-arch/master/my.conf > my.conf
 sh setup.sh /root/my.conf
 
