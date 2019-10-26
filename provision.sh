95853ad1697eefa8174a37a39140b82fbd2008da
chriskvik <christian@chk.no>
2019-10-27 01:20:10 +0200

    Create provision.sh

---
+++ provision.sh
@@ -0,0 +1,5 @@
+curl -sL https://raw.githubusercontent.com/chriskvik/provision-arch/master/arch-setup.sh > setup.sh
+curl -sL https://github.com/chriskvik/provision-arch/blob/master/configure.sh > configure.sh
+curl -sL https://github.com/chriskvik/provision-arch/blob/master/my.conf > my.conf
+sh setup.sh /root/my.conf
+
