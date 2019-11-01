130e19c27a9afc4c82f233f09904889a20d200ae
chriskvik <christian@chk.no>
2019-11-01 22:17:04 +0100

    Delete provision.sh

--- provision.sh
+++
@@ -1,5 +0,0 @@
-curl -sL https://raw.githubusercontent.com/chriskvik/provision-arch/master/arch-setup.sh > setup.sh
-curl -sL https://raw.githubusercontent.com/chriskvik/provision-arch/master/configure.sh > configure.sh
-curl -sL https://raw.githubusercontent.com/chriskvik/provision-arch/master/my.conf > my.conf
-sh setup.sh /root/my.conf
-
