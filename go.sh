356c5d987cc3a09a1e73e5d10f97d3c44c350108
chriskvik <christian@chk.no>
2019-11-02 12:25:57 +0100

    Update go.sh

--- go.sh
+++ go.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -sL https://raw.githubusercontent.com/chriskvik/provision-arch/master/setup.sh > setup.sh
-curl -sL https://raw.githubusercontent.com/chriskvik/provision-arch/master/chroot.sh > chroot.sh
-curl -sL https://raw.githubusercontent.com/chriskvik/provision-arch/master/firstboot.sh > firstboot.sh
+curl -H "Cached-Control: no-cache" -sL https://raw.githubusercontent.com/chriskvik/provision-arch/master/setup.sh > setup.sh
+curl -H "Cached-Control: no-cache" -sL https://raw.githubusercontent.com/chriskvik/provision-arch/master/chroot.sh > chroot.sh
+curl -H "Cached-Control: no-cache" -sL https://raw.githubusercontent.com/chriskvik/provision-arch/master/firstboot.sh > firstboot.sh
 
 chmod +x setup.sh chroot.sh firstboot.sh
 
