634bcc114463aba36ecdcae3acdc284620c58328
chriskvik <christian@chk.no>
2019-11-02 12:08:23 +0100

    Update go.sh

--- go.sh
+++ go.sh
@@ -2,7 +2,8 @@
 
 curl -sL https://raw.githubusercontent.com/chriskvik/provision-arch/master/setup.sh > setup.sh
 curl -sL https://raw.githubusercontent.com/chriskvik/provision-arch/master/chroot.sh > chroot.sh
+curl -sL https://raw.githubusercontent.com/chriskvik/provision-arch/master/firstboot.sh > firstboot.sh
 
-chmod +x setup.sh chroot.sh
+chmod +x setup.sh chroot.sh firstboot.sh
 
 sh setup.sh
