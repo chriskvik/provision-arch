7e9646485f5a50debb45e9ae614c1d80d1b02a10
chriskvik <christian@chk.no>
2019-11-03 19:12:33 +0100

    Update firstboot.sh

--- firstboot.sh
+++ firstboot.sh
@@ -43,36 +43,3 @@ sudo pacman -Syu gnupg2 pcsclite ccid hopenpgp-tools yubikey-personalization acs
 sudo systemctl enable pcscd.service
 sudo systemctl start pcscd.service 
 
-su $USERNAME -l << EOF
-  wget -P /home/$USERNAME/.gnupg https://raw.githubusercontent.com/drduh/config/master/gpg-agent.conf
-  wget -P /home/$USERNAME/.gnupg https://raw.githubusercontent.com/drduh/config/master/gpg.conf
-  chmod 600 /home/christian/.gnupg/gpg.conf
-EOF
-
-cat > /home/$USERNAME/.bashrc << EOF
-  export GPG_TTY="$(tty)"
-  export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
-  gpgconf --launch gpg-agent
-EOF
-
-su $USERNAME -l << EOF
-  source /home/$USERNAME/.bashrc
-  killall gpg-agent
-  gpg-connect-agent updatestartuptty /bye
-EOF
-
-# clone dotfiles
- su $USERNAME -l << EOF
-  cd ~
-  git init
-  git remote add origin git@github.com:chriskvik/dotfiles-arch.git
-  #git clean -f
-  git pull origin master
-EOF
-
-# run provision
-#cd ~$USERNAME/.install
-#make install
-
-# TODO: kill dhcpcd
-#poweroff
